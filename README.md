fdab83da9359ef01d372cf8c4edf48168b59eb5f
isabel <y2253li@uwaterloo.ca>
2021-05-09 14:39:09 -0400

    add tests and readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # shopify-code-challenge
+This repo is for shopify fall 2021 backend internship position
+### How to run
+Install python3 and flask```pip3 install flask```(or ```pip install flask```). 
+Clone the repo, and run it by using ```sudo python app.py```(or ```python app.py```). 
+Open ```http://127.0.0.1:8080/``` in browser.
+### What I did 
+Finish Add image(s) to the repository idea.<br />
+##### Features
+Click ```Choose Files``` button to upload one/bulk of images with .jpg/.png/.gif extensions. 
+Image larger than 1MB(1024*1024) will be refused. 
+Then click ```submit``` to upload images and show in page.
+##### Tests
+From ```tests``` folder, select one or many ```xxxSuccess```files(like jpgSuccess.jpg) at the same time to upload. (success) <br />
+From ```tests``` folder, select one or many ```xxxFail```files(like jpgFail.jpg) at the same time to upload. (fail as expected, and return 413 error)
+(because file is large than 1MB)<br />
+From ```tests``` folder, select ```jpeg1.jpeg```file at the same time to upload. (fail as expected, return 400 error)
+(because file does not have required extension)<br />
+
+
+
+
+
+
+ 
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import imghdr
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -16,23 +16,25 @@ def validate_image(stream):
         return None
     return '.' + (format if format != 'jpeg' else 'jpg')
 
+#get index page
 @app.route('/')
 def index():
     files = os.listdir(app.config['UPLOAD_PATH'])
     return render_template('index.html', files=files)
 
+#submit image
 @app.route('/', methods=['POST'])
 def upload_files():
-    uploaded_file = request.files['file']
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            abort(500)
-        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    for uploaded_file in request.files.getlist('file'):
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if (file_ext not in app.config['UPLOAD_EXTENSIONS']) or (file_ext != validate_image(uploaded_file.stream)):
+                return "Invalid image", 400
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return redirect(url_for('index'))
 
+#list submitted image
 @app.route("/uploads/<filename>",methods=["GET"])
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
